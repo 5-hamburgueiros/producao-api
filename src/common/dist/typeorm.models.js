401ae1d54ebe952e833a68df91a4a8ee8c2416a1
Marcos Garcia <marcos.mgarcia5@gmail.com>
2024-05-17 21:32:18 -0300

    feat: base producao

--- src/common/dist/typeorm.models.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.typeOrmEntities = void 0;
-var model_1 = require("@/infra/database/typerom/model");
-exports.typeOrmEntities = [
-    model_1.ProducaoModelTypeOrm
-];
